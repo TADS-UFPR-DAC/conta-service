6f4015c60e93634ccabc607c26d87a088341083b
Gabriel-Wojciechowksi <gabrielwo314@gmail.com>
2022-08-23 13:46:37 -0300

    Initial commit

---
+++ src/test/java/bantads/conta/model/Conta/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bantads.conta.model.Conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
